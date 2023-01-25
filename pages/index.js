25c7b5a6e927827b0f99872d9e78ab2fc9e109a6
Adam Pengh <adam.pengh@bloomreach.com>
2023-01-25 14:49:10 -0500

    update main index page

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,11 @@
-import {
-  Box,
-  styled
-} from '@mui/material';
 import SidebarLayout from 'src/layouts/SidebarLayout';
 import Head from 'next/head';
 
-const OverviewWrapper = styled(Box)(
-  ({ theme }) => `
-    overflow: auto;
-    background: ${theme.palette.common.white};
-    flex: 1;
-    overflow-x: hidden;
-`
-);
-
 export default function Overview() {
   return (
-    <OverviewWrapper>
-      <Head>
-        <title>UI Extensions</title>
-      </Head>
-    </OverviewWrapper>
+    <Head>
+      <title>Bloomreach Content SaaS UI Extensions</title>
+    </Head>
   );
 }
 
